e47342570f4e52eb304fc6fc15d6d01743c9c108
Rumcajs121 <z.miara45@gmail.com>
2024-03-11 17:03:25 +0100

    I finish exercise. I was modifie commit and restore my file. I change my code in program.cs file i restore this file and i change commit.

--- GitLearnBook/Program.cs
+++ GitLearnBook/Program.cs
@@ -6,3 +6,12 @@ List<int> list=new List<int>{1,2,3,4};
 foreach(var x in list){
 System.Console.WriteLine(x);
 }
+
+Dictionary<int,string> dic=new Dictionary<int, string>();
+dic.Add(1,"ala");
+dic.Add(2,"kot");
+dic.Add(3,"test");
+
+foreach(var i in dic){
+    System.Console.WriteLine(i);
+}

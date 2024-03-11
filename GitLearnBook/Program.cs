7baf712c0df330000313abc793e6ad2ec4e56df1
Rumcajs121 <z.miara45@gmail.com>
2024-03-11 06:28:21 +0100

    I prepare my labarotory execise for my task practical git commit --amend

--- GitLearnBook/Program.cs
+++ GitLearnBook/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+List<int> list=new List<int>{1,2,3,4};
+foreach(var x in list){
+System.Console.WriteLine(x);
+}

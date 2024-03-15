1659cbf8e890f2dee5c3b25fd0fc916f6b12772e
Rumcajs121 <z.miara45@gmail.com>
2024-03-15 06:17:17 +0100

    First commit i send my local repository to github.

--- GitLearnBook/Program.cs
+++ GitLearnBook/Program.cs
@@ -1,17 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-List<int> list=new List<int>{1,2,3,4};
-foreach(var x in list){
-System.Console.WriteLine(x);
+﻿
+var temperraturedaily=new DailyTemperature(2.0,15.0);
+System.Console.WriteLine($"Temeratura wynosi{temperraturedaily.HightTemp} oraz {temperraturedaily.LowTemp}");
+var FullName=new FullName{
+FirstName="Jacek",
+LastName="kaszanka"
+};
+var result=FullName.FirstName+""+FullName.LastName;
+System.Console.WriteLine(result);
+public record FullName{
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
 }
+public readonly record struct DailyTemperature(double HightTemp, double LowTemp );
 
-Dictionary<int,string> dic=new Dictionary<int, string>();
-dic.Add(1,"ala");
-dic.Add(2,"kot");
-dic.Add(3,"test");
-
-foreach(var i in dic){
-    System.Console.WriteLine(i);
-}

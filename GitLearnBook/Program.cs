a2fc37f00c5537dfea2a0b57553777f0ece9b9ab
Rumcajs121 <z.miara45@gmail.com>
2024-03-17 16:37:17 +0100

    I end two first exercise. I use c#. The first exercise i change int number for string and i calculate sum char.The second exercise i search int value for Collatz Sequence.

--- GitLearnBook/Program.cs
+++ GitLearnBook/Program.cs
@@ -1,15 +1,62 @@
-﻿
-var temperraturedaily=new DailyTemperature(2.0,15.0);
-System.Console.WriteLine($"Temeratura wynosi{temperraturedaily.HightTemp} oraz {temperraturedaily.LowTemp}");
-var FullName=new FullName{
-FirstName="Jacek",
-LastName="kaszanka"
-};
-var result=FullName.FirstName+""+FullName.LastName;
-System.Console.WriteLine(result);
-public record FullName{
-    public string FirstName {get; set;}
-    public string LastName {get; set;}
-}
-public readonly record struct DailyTemperature(double HightTemp, double LowTemp );
+﻿using System;
+var random= new Random();
+int randomNumber=random.Next(1,1001);
+System.Console.WriteLine($"Wylosowana liczba to: {randomNumber}");
+NumberConverter convert=new NumberConverter();
+string result=convert.GetFourDigitNumber(randomNumber);
+System.Console.WriteLine($"Zapis słowny liczby: {result}");
 
+string skipWhiteSpace=result.Replace(" ","");
+int countString= skipWhiteSpace.ToCharArray().Count();
+
+System.Console.WriteLine($"Długość ciągu znaków:{countString}");
+
+
+
+
+public class NumberConverter{
+public string[] One = { "","one", "two", "three", "four", "five", "six", "eight", "nine" };
+public string[] Ten = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+public string[] Tenth = { "", "","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+public string GetOneDigitNumber(int number)
+{
+    return this.One[number];
+}
+public string GetTwoDigitNumber(int number){
+    int second=0;
+   if (number < 10)
+   {
+      return this.GetOneDigitNumber(number);
+   }
+   else if (number >= 10 && number < 20)
+   {
+    second = Convert.ToInt32(number.ToString().Substring(1, 1));
+      return this.Ten[second];
+   }
+   int first = Convert.ToInt32(number.ToString().Substring(0, 1));
+   second = Convert.ToInt32(number.ToString().Substring(1, 1));
+    if(second != 0){
+        return Tenth[first]+"-"+One[second];
+    }
+    return Tenth[first]+One[second];
+    }
+public string GetThreeDigitNumber(int number){
+   int first = Convert.ToInt32(number.ToString().Substring(0, 1));
+   string secondhalf = "";
+   if (number.ToString().Length == 3)
+   {
+      secondhalf = this.GetTwoDigitNumber(Convert.ToInt32(number.ToString().Substring(1, 2)));
+      
+      secondhalf = string.IsNullOrEmpty(secondhalf) == false ? "and " + secondhalf : string.Empty;
+      return $"{this.One[first]} hundred {secondhalf}";
+   }
+   return GetTwoDigitNumber(number);
+}
+public string GetFourDigitNumber(int number){
+    if (number.ToString().Length == 4)
+   {
+    return " one thousand";
+   }
+   return this.GetThreeDigitNumber(number);
+}
+}
---
+++ GitLearnBook1/Program.cs
@@ -0,0 +1,24 @@
+﻿uint startNumber=0U;
+uint countChair=0U;
+
+for(var start = 1000000U; start>0; start--){
+ var n=start;
+ var i=1U;
+
+ while(n!=1){
+    if(n%2==0){
+        n/=2;
+    }else{
+        n=3*n+1;
+    }
+    i++;
+ }
+    if(i<countChair){
+        continue;
+    }
+    startNumber=start;
+    countChair=i;
+}
+var answer=startNumber;
+
+System.Console.WriteLine($"Wynik dla warunków w zadaniu to: {answer}");
